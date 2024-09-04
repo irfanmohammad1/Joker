de1d09804672dc2edf0ceae444ba1fa2e4920651
Mohammad Irfan <133006092+irfanmohammad1@users.noreply.github.com>
2024-09-04 22:55:07 +0530

    Create App.jsx
    App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Form from './Form';
+import CommentsForm from './CommentsForm';
+import Counter from './Counter';
+import Joker from './Joker';
+
+function App() {
+  return(
+    <>
+     <Joker/>
+    </>
+  )};
+
+export default App;
